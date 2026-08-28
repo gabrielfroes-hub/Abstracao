1c3c0b71b52592be166b42f4a7d5946e1779a6e0
gabrielfroes-hub <gabriel.froes@edu.senai.br>
2026-08-28 10:44:43 -0300

    Atividade 2

---
+++ Aluno.cs
@@ -0,0 +1,29 @@
+﻿namespace Abstracao
+{
+    internal class Aluno
+    {
+        public string nome;
+        public string matricula;
+        public int Nota1;
+        public int Nota2;
+        double media;
+        public double calcularMedia()
+        {
+            media = (Nota1 + Nota2) / 2.0;
+            return media;
+        }
+
+        public void exibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Matrícula: {matricula}");
+            Console.WriteLine($"Nota 1: {Nota1}");
+            Console.WriteLine($"Nota 2: {Nota2}");
+            Console.WriteLine($"Média: {calcularMedia()}");
+            Console.WriteLine("-----------------------");
+        }
+
+
+
+    }
+}
